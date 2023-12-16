2b9affe3d2f0ab4fba472d6455e49aaf3a51fa57
Vlad Krupen <air7360@gmail.com>
2023-12-16 12:29:34 +0300

    Each word has its own color

--- Homework_AttributedString/ViewController.swift
+++ Homework_AttributedString/ViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textView.text = "Каждый охотник желает знать где сидит фазан"
+        
+        let arrayWord: [String] = textView.text.components(separatedBy: " ")
+        print(arrayWord)
+        
+        let arrayColor: [UIColor] = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.systemBlue, UIColor.blue, UIColor.purple]
+        
+        let attributedString = NSMutableAttributedString(string: textView.text)
+        
+        attributedString.addAttribute(.font, value: UIFont(name: "RubikLines-Regular", size: 40)!, range: NSRange(location: 0, length: textView.text.count))
+        
+        
+        for num in 0..<arrayWord.count {
+            if let location = textView.text.firstRange(of: "\(arrayWord[num])") {
+                let lowerBound = location.lowerBound.encodedOffset
+                let length = location.upperBound.encodedOffset - lowerBound
+                attributedString.addAttribute(.foregroundColor, value: arrayColor[num], range: NSRange(location: lowerBound, length: length))
+            }
+        }
+                                      
+        
+        textView.attributedText = attributedString
+        
+
     }
 
 
